57e1bdf3a1268f421b101773a619308f88aa799c
Frederick Johnson <fritzj96@gmail.com>
2021-07-30 13:47:56 -0500

    cleanup

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import { Layout } from "../components/Layout";
 export default function Hiring() {
   return (
     <Layout
-      title="Fritz Johnson / About Fritz"
+      title="Fritz Johnson / About"
       description="Learn a little more about me as a developer & as a person here on the aptly-named 'about' page"
     />
   );
